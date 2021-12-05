9548ca7f893863f78f63d932c3d304b0abbb16f0
Ruhul <ruhul98@gmail.com>
2021-12-05 09:53:10 +0530

    first commit

---
+++ src/test/java/com/ruhul/springgithubloginstarter/SpringGithubLoginStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ruhul.springgithubloginstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGithubLoginStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
